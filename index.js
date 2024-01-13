5c82f56289f5f1fe85e869e211e2ddc478641b69
Emesiana Celestine <celelove4all@gmail.com>
2024-01-13 12:45:05 +0100

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendCat(){
+   cats.push("Ralph")
+}
+
+
+function destructivelyPrependCat(){
+  cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+
+function appendCat(){
+   return appendCat = [...cats, "Broom"]
+}
+
+function prependCat(){
+  return prependCat = ["Arnold", ...cats]
+}
+
+
+function removeLastCat(){
+  return cats.slice(0, 2)
+}
+
+
+function removeFirstCat(){
+  return cats.slice(1)
+}
